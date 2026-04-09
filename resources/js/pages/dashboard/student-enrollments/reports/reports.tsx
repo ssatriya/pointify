1545882af7f537cde886cc83822d484fb4dbbb7d
ssatriya <ssatriya@gmail.com>
2026-04-09 22:16:53 +0800

    style: center chart loader

--- resources/js/pages/dashboard/student-enrollments/reports/reports.tsx
+++ resources/js/pages/dashboard/student-enrollments/reports/reports.tsx
@@ -1,8 +1,10 @@
 import { Head } from "@inertiajs/react";
 import ClassLayout from "@/components/layout/class-layout";
 import type { Class } from "@/types";
+import { lazy, Suspense } from "react";
+import { Loader } from "lucide-react";
 
-// const ClassOverviewChart = lazy(() => import('../partials/class-overview-chart'))
+const ClassOverviewChart = lazy(() => import('../partials/class-overview-chart'))
 
 type Props = {
     studentClass: Class;
@@ -20,9 +22,13 @@ export default function Reports({ studentClass, classOverview }: Props) {
     return (
         <>
             <Head title={`Laporan Kelas ${studentClass.name}`} />
-            {/* <Suspense fallback={<div><Loader className="h-6 w-6 animate-spin" /> </div>}>
+            <Suspense fallback={
+                <div className="flex h-96 w-full items-center justify-center">
+                    <Loader className="h-6 w-6 animate-spin text-muted-foreground" />
+                </div>
+            }>
                 <ClassOverviewChart studentClass={studentClass} classOverview={classOverview} />
-            </Suspense> */}
+            </Suspense>
         </>
     );
 }
